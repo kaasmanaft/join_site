79fb2c067b14dc6ed9658e654281d48bb80cec7e
Dzmitry Romanchuk <rdaventura@tut.by>
2019-07-31 10:45:35 +0200

    add Order template

--- mysite/Order/models.py
+++ mysite/Order/models.py
@@ -3,8 +3,10 @@ from django.contrib.auth.models import User
 
 
 class Order(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     item_name = models.CharField(max_length=100)
     price = models.FloatField()
     amount = models.IntegerField()
+
+
 
--- mysite/Order/views.py
+++ mysite/Order/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Order
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+
