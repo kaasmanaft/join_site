e2ae7d2349471912a949ae7821caf1b7d623761c
Dzmitry Romanchuk <rdaventura@tut.by>
2019-08-08 12:02:23 +0200

    Order is_valid() method save_order() method update order if it exist

--- mysite/Order/models.py
+++ mysite/Order/models.py
@@ -14,11 +14,22 @@ class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     item_id = models.PositiveIntegerField(null=True)
     price = models.FloatField()
-    quantity = models.IntegerField()
+    quantity = models.PositiveIntegerField()
     status = models.CharField(choices=order_status, default='ACT', max_length=5)
     order_date = models.DateField(default=timezone.now)
     delivery_date = models.DateField(null=True)
     is_partner_goods = models.BooleanField(default=False)
+
+    def is_valid(self):
+        if int(self.price) <= 0:
+            return False
+        if int(self.quantity) <= 0:
+            return False
+        if int(self.item_id) <= 0:
+            return False
+        if self.user is None:
+            return False
+        return True
 
 
 
--- mysite/Order/views.py
+++ mysite/Order/views.py
@@ -2,8 +2,10 @@ from typing import List, Dict
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Order
+# from .forms import OrderForm
 from django.contrib.auth.models import User
 from django.db import connections
+
 
 def dictfetchall(cursor) -> List:
     "Return all rows from a cursor as a dict"
@@ -11,7 +13,8 @@ def dictfetchall(cursor) -> List:
     return [
         dict(zip(columns, row))
         for row in cursor.fetchall()
-    ]
+          ]
+
 
 def db_goods_view_tmp(request, limit=10):
     if request.method == "POST":
@@ -20,12 +23,12 @@ def db_goods_view_tmp(request, limit=10):
         return redirect('customers')
     else:
         with connections['goods'].cursor() as cursor:
-            cursor.execute('SELECT id,agg_photos,base_photo_url, description,min_qty, name, price FROM item LIMIT %s;',
+            cursor.execute('SELECT id,agg_photos,base_photo_url, description,min_qty, name, price FROM item '
+                           'where agg_photos is not null LIMIT %s;',
                            [limit])
             dbd = dictfetchall(cursor)
 
             return render(request, template_name='Order/db.html', context={'dbd': dbd})
-
 
 
 def get_item_info(item_id: int) -> Dict:
@@ -37,28 +40,37 @@ def get_item_info(item_id: int) -> Dict:
     return item_info[0]
 
 
-
 @login_required()
 def save_order(request):
     if request.method == "POST":
-        new_order = Order()
-        new_order.item_id = request.POST['id']
-        item_info = get_item_info(new_order.item_id)
-        new_order.user = request.user
-        new_order.quantity = int(request.POST['quantity'])
-        # new_order.delivery_date = None
-        new_order.price = item_info['price']
-        new_order.is_partner_goods = item_info['is_remote_store']
-        new_order.save()
-    return redirect('db', 30)
+        item = request.POST['id']
+        try:
+            order = Order.objects.get(item_id__exact=int(item), user__username__exact=request.user)
+            print(order)
+
+        except Order.DoesNotExist:
+            print('There is no any orders with that ID')
+            new_order = Order()
+            new_order.item_id = request.POST['id']
+            item_info = get_item_info(new_order.item_id)
+            new_order.user = request.user
+            new_order.quantity = request.POST['quantity']
+            new_order.price = item_info['price']
+            new_order.is_partner_goods = item_info['is_remote_store']
+            if new_order.is_valid():
+                new_order.save()
+                return redirect('db', 30)
+            else:
+                print(repr(new_order))
+                return redirect('item', new_order.item_id)
+        return redirect('db', 30)
+    else:
+        return redirect('db', 30)
 
 
 def show_goods_by_id(request, id=0):
-    if request.method == "POST":
-        print(request.user)
-        print(request.POST)
-        quantity = request.POST['quantity']
-        with connections['goods'].cursor() as cursor:
-            cursor.execute("SELECT id,agg_photos,base_photo_url, description,min_qty, name, price FROM item WHERE id=%s;",[id])
-            dbd = dictfetchall(cursor)
-            return render(request, template_name='Order/db.html', context={'dbd': dbd,'quantity': quantity})
+    with connections['goods'].cursor() as cursor:
+        cursor.execute("SELECT id,agg_photos,base_photo_url, description,min_qty, name, price FROM item WHERE id=%s;",
+                       [id])
+        dbd = dictfetchall(cursor)
+        return render(request, template_name='Order/db.html', context={'dbd': dbd, 'quantity': 1})
